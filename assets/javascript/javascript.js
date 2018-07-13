b2d96f36e2f16c478c84b9c75eed495a3ee18844
jpeek01 <jpeek01@gmail.com>
2018-07-12 23:44:16 -0500

    got the timers working. added the buttons and click functionality. This thing is starting to actually work

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -7,7 +7,7 @@ var game = {
     questionNumber: 0,
 
     questionStartTimer: function() {
-        game.questionTimeOut = 10;
+        game.questionTimeOut = 20;
         game.questionIntervalId = setInterval(game.questionCountDown, 1000);
     },
 
@@ -30,9 +30,11 @@ var game = {
 
     answerTimeOut: 0,
     answerIntervalId: 0,
+    userAnswer: '',
+    test: 1,
 
     answerStartTimer: function() {
-        game.answerTimeOut = 5;
+        game.answerTimeOut = 10;
         game.answerIntervalId = setInterval(game.answerCountDown, 1000);
     },
 
@@ -40,7 +42,7 @@ var game = {
         game.answerTimeOut--;
         $('#timer').text(game.answerTimeOut);
 
-        if (game.answerTimeOut === 0) {
+        if (game.answerTimeOut == 0) {
             game.stopAnswerCountDown();
             game.nextQuestion();
         }
@@ -50,18 +52,40 @@ var game = {
         clearInterval(game.answerIntervalId);
     },
 
-    showAnswer: function() {
+    showAnswer: function(answer) {
         game.answerStartTimer();
+
+        if (answer == questions.answer) {
+            game.stopAnswerCountDown();
+            game.nextQuestion();
+        }
+        // btn-success
+        // secondary
     },
 
-    nextQuestion: function() {
-        game.questionStartTimer();
-        console.log(game.questionNumber);
-        game.questionNumber++;
-        if (game.questionNumber >= questions.length-1) {
-            game.stopAnswerCountDown();
+    nextQuestion: function() {     
+        console.log("next question " + game.questionNumber + ' ' + questions.length);
+
+        if (game.questionNumber == questions.length-1) {
+            console.log("game over");
+            game.gameOver();
+        } else {
+            game.questionNumber++;
+            game.questionStartTimer();
+            game.displayQuestion();
         }
-        game.displayQuestion();
+        
+    },
+
+    gameOver: function() {
+        game.stopAnswerCountDown();
+        game.stopQuestionCountDown();
+
+        $('#question').html("<h2>Game Over!<h2>");
+        $('#a').hide();
+        $('#b').hide();
+        $('#c').hide();
+        $('#d').hide();
     },
 
     displayQuestion: function() {
@@ -70,6 +94,10 @@ var game = {
         $('#b').text(questions[game.questionNumber].b);
         $('#c').text(questions[game.questionNumber].c);
         $('#d').text(questions[game.questionNumber].d);
+    },
+
+    displayAnswer: function() {
+
     }
 }
 
@@ -90,8 +118,14 @@ var questions = [
 ]
 
 $(document).ready(function() {
-
+    var userAnswer;
     game.questionStartTimer();
     game.displayQuestion();
+
+    $("button").on("click", function() {
+        userAnswer = $(this).val()
+        game.showAnswer()
+
+    });
 
 });
